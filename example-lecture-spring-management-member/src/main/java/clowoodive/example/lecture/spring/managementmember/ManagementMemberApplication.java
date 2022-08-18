1410bd224d8544b9a298c2c079a1970fc53e0e80
clowoodive <clowoodive@gmail.com>
2022-08-18 14:15:36 +0900

    Feat: Add "example-lecture-spring-management-member" project

---
+++ example-lecture-spring-management-member/src/main/java/clowoodive/example/lecture/spring/managementmember/ManagementMemberApplication.java
@@ -0,0 +1,13 @@
+package clowoodive.example.lecture.spring.managementmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementMemberApplication.class, args);
+	}
+
+}
---
+++ example-lecture-spring-management-member/src/test/java/clowoodive/example/lecture/spring/managementmember/ManagementMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package clowoodive.example.lecture.spring.managementmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
