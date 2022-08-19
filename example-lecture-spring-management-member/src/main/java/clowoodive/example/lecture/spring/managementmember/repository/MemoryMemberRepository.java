6165b6f0fc1acbc71390ecb31bbb8fd41c18de8c
clowoodive <clowoodive@gmail.com>
2022-08-20 01:19:26 +0900

    Feat: Add service logic & test code

---
+++ example-lecture-spring-management-member/src/main/java/clowoodive/example/lecture/spring/managementmember/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package clowoodive.example.lecture.spring.managementmember.repository;
+
+import clowoodive.example.lecture.spring.managementmember.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();  // 동시성 문제를 야기 할 수 있기에 실무에서는 ConcurrentHashMap 써야함
+    private static long sequence = 0L;  // 동시성 문제를 야기 할 수 있기에 실무에서는 AtomicLong 써야함
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ example-lecture-spring-management-member/src/test/java/clowoodive/example/lecture/spring/managementmember/service/MemberServiceTest.java
@@ -0,0 +1,78 @@
+package clowoodive.example.lecture.spring.managementmember.service;
+
+import clowoodive.example.lecture.spring.managementmember.domain.Member;
+import clowoodive.example.lecture.spring.managementmember.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    //    MemoryMemberRepository memberRepository = new MemoryMemberRepository(); // 내부 필드가 static 필드라 clearStore()가 문제가 없지만 애매하다. 따라서 의존성 주입하도록 변경하고
+    //    beforeEach추가
+    MemoryMemberRepository memberRepository;
+
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void duplicateJoin() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+/*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+*/
+
+        // then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
