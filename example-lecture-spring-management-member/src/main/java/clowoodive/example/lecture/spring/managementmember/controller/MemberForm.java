5a8cbb9d8653e54bcf1ed572156776a852a86076
clowoodive <clowoodive@gmail.com>
2022-08-23 13:22:17 +0900

    Feat: Add member register & list code

---
+++ example-lecture-spring-management-member/src/main/java/clowoodive/example/lecture/spring/managementmember/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package clowoodive.example.lecture.spring.managementmember.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
