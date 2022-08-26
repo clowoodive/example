762a4dfb304a1e843ed4d7008c40185800cf3bf6
clowoodive <clowoodive@gmail.com>
2022-08-27 00:21:08 +0900

    Feat: AOP 프록시 클래스 확인 코드 추가

--- example-lecture-spring-management-member/src/main/java/clowoodive/example/lecture/spring/managementmember/controller/MemberController.java
+++ example-lecture-spring-management-member/src/main/java/clowoodive/example/lecture/spring/managementmember/controller/MemberController.java
@@ -18,6 +18,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+        System.out.println("memberService = " + memberService.getClass()); // AOP가 적용되어 proxy 클래스인 것을 확인 가능
     }
 
     @GetMapping("/members/new")
