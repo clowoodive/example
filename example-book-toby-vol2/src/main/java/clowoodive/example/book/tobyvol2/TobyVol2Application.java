7f549c66f3e12e020b12d29648cefda50db26ad1
clowoodive <clowoodive@gmail.com>
2023-02-22 01:14:22 +0900

    feat: 토비의 스프링 예제 코드 프로젝트 생성

---
+++ example-book-toby-vol2/src/main/java/clowoodive/example/book/tobyvol2/TobyVol2Application.java
@@ -0,0 +1,13 @@
+package clowoodive.example.book.tobyvol2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyVol2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobyVol2Application.class, args);
+	}
+
+}
---
+++ example-book-toby-vol2/src/test/java/clowoodive/example/book/tobyvol2/TobyVol2ApplicationTests.java
@@ -0,0 +1,13 @@
+package clowoodive.example.book.tobyvol2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyVol2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
