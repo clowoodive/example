490bbed55b0a59dacec29dd3c1aa6d2b01703beb
clowoodive <clowoodive@gmail.com>
2023-04-13 13:54:06 +0900

    Feat: 디자인 패턴 - 템플릿 메서드 예제 추가

---
+++ example-design-pattern/src/main/java/clowoodive/example/designpattern/behavior/TemplateMethodClass.java
@@ -0,0 +1,59 @@
+package clowoodive.example.designpattern.behavior;
+
+public abstract class TemplateMethodClass {
+    public final void templateMethod() {
+        step1();  // 공통 로직
+        step2();  // 공통 로직
+        hookMethod();  // 선택 오버라이드
+        step3();  // 필수 오버라이드
+        step4();  // 공통 로직
+    }
+
+    protected void step1() {
+        System.out.println("template step1");
+    }
+
+    protected void step2() {
+        System.out.println("template step2");
+    }
+
+    protected void hookMethod() {
+        System.out.println("template hookMethod");
+    }
+
+    protected abstract void step3();
+
+    protected void step4() {
+        System.out.println("template step4");
+    }
+}
+
+class ConcreteClass1 extends TemplateMethodClass {
+
+    // hookMethod가 아니어도 필요에 따라 선택 오버라이드
+    @Override
+    public void step2() {
+        System.out.println("concrete1 step2");
+    }
+
+    // 필수 오버라이드
+    @Override
+    public void step3() {
+        System.out.println("concrete1 step3");
+    }
+}
+
+class ConcreteClass2 extends TemplateMethodClass {
+
+    // 선택 오버라이드
+    @Override
+    protected void hookMethod() {
+        System.out.println("concrete2 hookMethod");
+    }
+
+    // 필수 오버라이드
+    @Override
+    public void step3() {
+        System.out.println("concrete2 step3");
+    }
+}
---
+++ example-design-pattern/src/test/java/clowoodive/example/designpattern/behavior/TemplateMethodClassTest.java
@@ -0,0 +1,18 @@
+package clowoodive.example.designpattern.behavior;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemplateMethodClassTest {
+
+    @Test
+    void templateMethod() {
+        System.out.println("----- concrete1 -----");
+        TemplateMethodClass concrete1 = new ConcreteClass1();
+        concrete1.templateMethod();
+        System.out.println("----- concrete2 -----");
+        TemplateMethodClass concrete2 = new ConcreteClass2();
+        concrete2.templateMethod();
+    }
+}
