3ac6f4b6144715ef3f8e69b5bd8206bb72eba1b0
clowoodive <clowoodive@gmail.com>
2023-04-13 13:53:19 +0900

    Chore: 디자인 패턴 예제코드 프로젝트 생성

---
+++ example-design-pattern/src/main/java/clowoodive/example/designpattern/DesighPatternApplication.java
@@ -0,0 +1,13 @@
+package clowoodive.example.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesighPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesighPatternApplication.class, args);
+	}
+
+}
