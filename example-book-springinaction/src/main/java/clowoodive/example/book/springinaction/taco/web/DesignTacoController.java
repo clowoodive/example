4abcf8eee1ad2538bff548718754c0a253943643
clowoodive <clowoodive@gmail.com>
2023-10-06 15:41:17 +0900

    Feat: Model Validation 추가

--- example-book-springinaction/src/main/java/clowoodive/example/book/springinaction/taco/web/DesignTacoController.java
+++ example-book-springinaction/src/main/java/clowoodive/example/book/springinaction/taco/web/DesignTacoController.java
@@ -6,10 +6,12 @@ import clowoodive.example.book.springinaction.taco.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,7 +34,7 @@ public class DesignTacoController {
                 new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
                 new Ingredient("SLSA", "Salsa", Type.SAUCE),
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-                );
+        );
 
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
@@ -51,7 +53,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "redirect:/design";
+        }
+
         // 식자재 내역 저장(추후)
         log.info("Processing design: " + design);
 
